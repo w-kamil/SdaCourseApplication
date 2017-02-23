87a7479d0e42981853261ca69bfce7861ff9dc84
w-kamil <w-kamil@wp.pl>
2017-02-23 10:47:07 +0100

    mainactivity note added

--- app/src/main/java/com/hfad/sdacourseapplication/MainActivity.java
+++ app/src/main/java/com/hfad/sdacourseapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.hfad.sdacourseapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hfad.sdacourseapplication.drawapp.DrawAppActivity;
@@ -17,6 +21,7 @@ import com.hfad.sdacourseapplication.simpledrawgame.SimpleDrawingGameMainActivit
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NOTES_KEY = "notes";
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -24,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.open);
@@ -66,7 +70,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final EditText notesEditText = (EditText) findViewById(R.id.my_note_edit_text);
+        notesEditText.setText(readText());
 
+        Button saveButton = (Button) findViewById(R.id.save_note_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText(notesEditText.getText().toString());
+            }
+        });
+    }
+
+
+    private String readText(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return sharedPreferences.getString(NOTES_KEY, "");
+    }
+
+    private void saveText(String text){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putString(NOTES_KEY, text).apply();
     }
 
     @Override
